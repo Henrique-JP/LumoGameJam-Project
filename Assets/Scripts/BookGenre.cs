8e8cac6465af8cac534967c739f56e1f564ffb05
Marina B <marinabarbosa4@gmail.com>
2025-06-26 23:39:45 -0300

    Generos literarios para classificar cada fantasma

---
+++ Assets/Scripts/BookGenre.cs
@@ -0,0 +1,10 @@
+    // Este enum define os tipos de gênero possíveis para fantasmas e livros.
+    // Não é necessário anexar este script a nenhum objeto na cena.
+    public enum TipoGenero
+    {
+        Nenhum, // Útil para quando o jogador não tem nenhum livro equipado
+        Terror,
+        Fantasia,
+        Romance
+    }
+
